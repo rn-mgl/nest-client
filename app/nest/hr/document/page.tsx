515b233c6cc68bb2368da69137be266986d3ed2d
rn-mgl <reinb2ra@gmail.com>
2025-02-03 19:57:45 +0800

    handle active menu

--- app/nest/hr/document/page.tsx
+++ app/nest/hr/document/page.tsx
@@ -18,7 +18,10 @@ import { IoAdd, IoEllipsisVertical, IoPencil, IoTrash } from "react-icons/io5";
 const HRDocument = () => {
   const [canCreateDocument, setCanCreateDocument] = React.useState(false);
   const [canCreateFolder, setCanCreateFolder] = React.useState(false);
-  const [activeDocumentMenu, setActiveDocumentMenu] = React.useState(0);
+  const [activeDocumentMenu, setActiveDocumentMenu] = React.useState<{
+    type: string;
+    id: number;
+  }>({ type: "", id: 0 });
   const [documents, setDocuments] = React.useState<
     Array<
       | (DocumentInterface & UserInterface)
@@ -38,8 +41,14 @@ const HRDocument = () => {
     setCanCreateFolder((prev) => !prev);
   };
 
-  const handleActiveDocumentMenu = (id: number) => {
-    setActiveDocumentMenu((prev) => (prev !== id ? id : 0));
+  const handleActiveDocumentMenu = (type: string, id: number) => {
+    setActiveDocumentMenu((prev) => {
+      const defaultSet = { type: "", id: 0 };
+      const newSet =
+        prev.type === type && prev.id === id ? defaultSet : { type, id };
+
+      return newSet;
+    });
   };
 
   const getDocuments = React.useCallback(async () => {
@@ -69,8 +78,11 @@ const HRDocument = () => {
   }, [url, user?.token]);
 
   const mappedDocuments = documents.map((document, index) => {
-    const isDocument = "type" in document && document.type !== "folder";
+    const type = "type" in document ? document.type : "folder";
+    const isDocument = type !== "folder";
     const createdBy = document.created_by === user?.current;
+    const activeMenu =
+      type === activeDocumentMenu.type && document.id === activeDocumentMenu.id;
 
     return isDocument ? (
       <div
@@ -86,14 +98,14 @@ const HRDocument = () => {
           </div>
 
           <button
-            onClick={() => document.id && handleActiveDocumentMenu(document.id)}
+            onClick={() =>
+              document.id && handleActiveDocumentMenu(type, document.id)
+            }
             className="p-2 rounded-full bg-neutral-100 transition-all"
           >
             <IoEllipsisVertical
               className={`${
-                activeDocumentMenu === document.id
-                  ? "text-accent-blue"
-                  : "text-neutral-900"
+                activeMenu ? "text-accent-blue" : "text-neutral-900"
               }`}
             />
           </button>
@@ -105,10 +117,10 @@ const HRDocument = () => {
           </p>
         </div>
 
-        {activeDocumentMenu === document.id ? (
+        {activeMenu ? (
           <div className="w-32 p-2 rounded-md top-12 right-6 shadow-md bg-neutral-200 absolute animate-fade z-20">
             <button
-              // onClick={handleCanEditLeave}
+              // onClick={handleCanEditDocument}
               className="w-full p-1 rounded-sm text-sm bg-neutral-200 transition-all flex flex-row gap-2 items-center justify-start"
             >
               <IoPencil className="text-accent-blue" />
@@ -117,7 +129,7 @@ const HRDocument = () => {
 
             {createdBy ? (
               <button
-                // onClick={handleCanDeleteLeave}
+                // onClick={handleCanDeleteDocument}
                 className="w-full p-1 rounded-sm text-sm bg-neutral-200 transition-all flex flex-row gap-2 items-center justify-start"
               >
                 <IoTrash className="text-red-600" />
@@ -128,9 +140,59 @@ const HRDocument = () => {
         ) : null}
       </div>
     ) : (
-      <Link key={index} href={`/nest/hr/document/${document.id}`}>
-        {document.name}
-      </Link>
+      <div
+        key={index}
+        className="w-full h-full p-4 rounded-md bg-neutral-100 flex flex-col items-start justify-start gap-4 relative  max-h-56 max-w-full"
+      >
+        <div className="flex flex-row items-start justify-between w-full">
+          <div className="flex flex-col items-start justify-start">
+            <Link
+              href={`/nest/hr/document/${document.id}`}
+              className="font-bold truncate hover:underline"
+            >
+              {document.name}
+            </Link>
+            <p className="text-xs">
+              created by {createdBy ? "you" : `${document.first_name}`}
+            </p>
+          </div>
+
+          <button
+            onClick={() =>
+              document.id && handleActiveDocumentMenu(type, document.id)
+            }
+            className="p-2 rounded-full bg-neutral-100 transition-all"
+          >
+            <IoEllipsisVertical
+              className={`${
+                activeMenu ? "text-accent-blue" : "text-neutral-900"
+              }`}
+            />
+          </button>
+
+          {activeMenu ? (
+            <div className="w-32 p-2 rounded-md top-12 right-6 shadow-md bg-neutral-200 absolute animate-fade z-20">
+              <button
+                // onClick={handleCanEditDocument}
+                className="w-full p-1 rounded-sm text-sm bg-neutral-200 transition-all flex flex-row gap-2 items-center justify-start"
+              >
+                <IoPencil className="text-accent-blue" />
+                Edit
+              </button>
+
+              {createdBy ? (
+                <button
+                  // onClick={handleCanDeleteDocument}
+                  className="w-full p-1 rounded-sm text-sm bg-neutral-200 transition-all flex flex-row gap-2 items-center justify-start"
+                >
+                  <IoTrash className="text-red-600" />
+                  Delete
+                </button>
+              ) : null}
+            </div>
+          ) : null}
+        </div>
+      </div>
     );
   });
 
@@ -176,10 +238,10 @@ const HRDocument = () => {
             Create Folder
             <IoAdd className="text-lg" />
           </button>
+        </div>
 
-          <div className="w-full grid grid-cols-1 gap-4 t:grid-cols-2 l-l:grid-cols-3">
-            {mappedDocuments}
-          </div>
+        <div className="w-full grid grid-cols-1 gap-4 t:grid-cols-2 l-l:grid-cols-3">
+          {mappedDocuments}
         </div>
       </div>
     </div>
