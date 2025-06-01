9739dced169ac2d8fc39badd1d96e47cce11e7f4
rn-mgl <reinb2ra@gmail.com>
2025-06-01 20:19:44 +0800

    remove email update

--- src/components/admin/profile/EditProfile.tsx
+++ src/components/admin/profile/EditProfile.tsx
@@ -6,10 +6,10 @@ import { UserInterface } from "@/src/interface/UserInterface";
 import { getCSRFToken } from "@/src/utils/token";
 import axios from "axios";
 import { useSession } from "next-auth/react";
-import React from "react";
-import { IoClose, IoImage, IoMail, IoPerson, IoTrash } from "react-icons/io5";
-import Input from "../../form/Input";
 import Image from "next/image";
+import React from "react";
+import { IoClose, IoImage, IoPerson, IoTrash } from "react-icons/io5";
+import Input from "../../form/Input";
 
 const EditAdminProfile: React.FC<ModalInterface> = (props) => {
   const [profile, setProfile] = React.useState<
