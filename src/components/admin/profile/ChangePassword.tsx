0ea45f80f2201ed952dc67adea64ccaf9ea39d41
rn-mgl <reinb2ra@gmail.com>
2025-06-08 13:03:18 +0800

    can change password

--- src/components/admin/profile/ChangePassword.tsx
+++ src/components/admin/profile/ChangePassword.tsx
@@ -1,7 +1,11 @@
 import { ModalInterface } from "@/src/interface/ModalInterface";
 import React from "react";
-import { IoClose } from "react-icons/io5";
+import { IoClose, IoEye, IoEyeOff } from "react-icons/io5";
 import Input from "../../form/Input";
+import axios from "axios";
+import { useSession } from "next-auth/react";
+import { getCSRFToken } from "@/src/utils/token";
+import { useRouter } from "next/navigation";
 
 const ChangePassword: React.FC<ModalInterface> = (props) => {
   const [password, setPassword] = React.useState({
@@ -9,6 +13,16 @@ const ChangePassword: React.FC<ModalInterface> = (props) => {
     new_password: "",
     new_password_confirmation: "",
   });
+  const [showPassword, setShowPassword] = React.useState({
+    current_password: false,
+    new_password: false,
+    new_password_confirmation: false,
+  });
+
+  const url = process.env.URL;
+  const { data: session } = useSession({ required: true });
+  const user = session?.user;
+  const router = useRouter();
 
   const handlePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
@@ -19,6 +33,42 @@ const ChangePassword: React.FC<ModalInterface> = (props) => {
         [name]: value,
       };
     });
+  };
+
+  const handleShowPassword = (name: string) => {
+    setShowPassword((prev) => {
+      return {
+        ...prev,
+        [name]: !prev[name as keyof object],
+      };
+    });
+  };
+
+  const submitChangePassword = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const { token } = await getCSRFToken();
+
+      if (token && user?.token) {
+        const { data: responseData } = await axios.post(
+          `${url}/admin/auth/change_password`,
+          { ...password },
+          {
+            headers: {
+              Authorization: `Bearer ${user?.token}`,
+              "X-CSRF-TOKEN": token,
+            },
+            withCredentials: true,
+          }
+        );
+
+        if (responseData.success) {
+          router.push("/control/login");
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
@@ -36,15 +86,31 @@ const ChangePassword: React.FC<ModalInterface> = (props) => {
             <IoClose />
           </button>
         </div>
-        <form className="w-full flex flex-col items-center justify-start gap-4 p-4">
+        <form
+          onSubmit={(e) => submitChangePassword(e)}
+          className="w-full flex flex-col items-center justify-start gap-4 p-4"
+        >
           <Input
             id="current_password"
             onChange={handlePassword}
             placeholder="Current Password"
             required={true}
-            type="password"
+            type={showPassword.current_password ? "text" : "password"}
             value={password.current_password}
             label={true}
+            icon={
+              showPassword.current_password ? (
+                <IoEyeOff
+                  onClick={() => handleShowPassword("current_password")}
+                  className="cursor-pointer"
+                />
+              ) : (
+                <IoEye
+                  onClick={() => handleShowPassword("current_password")}
+                  className="cursor-pointer"
+                />
+              )
+            }
           />
 
           <Input
@@ -52,9 +118,22 @@ const ChangePassword: React.FC<ModalInterface> = (props) => {
             onChange={handlePassword}
             placeholder="New Password"
             required={true}
-            type="password"
+            type={showPassword.new_password ? "text" : "password"}
             value={password.new_password}
             label={true}
+            icon={
+              showPassword.new_password ? (
+                <IoEyeOff
+                  onClick={() => handleShowPassword("new_password")}
+                  className="cursor-pointer"
+                />
+              ) : (
+                <IoEye
+                  onClick={() => handleShowPassword("new_password")}
+                  className="cursor-pointer"
+                />
+              )
+            }
           />
 
           <Input
@@ -62,9 +141,26 @@ const ChangePassword: React.FC<ModalInterface> = (props) => {
             onChange={handlePassword}
             placeholder="Confirm New Password"
             required={true}
-            type="password"
+            type={showPassword.new_password_confirmation ? "text" : "password"}
             value={password.new_password_confirmation}
             label={true}
+            icon={
+              showPassword.new_password_confirmation ? (
+                <IoEyeOff
+                  onClick={() =>
+                    handleShowPassword("new_password_confirmation")
+                  }
+                  className="cursor-pointer"
+                />
+              ) : (
+                <IoEye
+                  onClick={() =>
+                    handleShowPassword("new_password_confirmation")
+                  }
+                  className="cursor-pointer"
+                />
+              )
+            }
           />
 
           <button className="w-full p-2 rounded-md bg-accent-yellow font-bold text-accent-blue mt-2">
