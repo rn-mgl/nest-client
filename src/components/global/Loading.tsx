dba04007761311bc50fccdf5f8ca9a8db92ea6be
rn-mgl <reinb2ra@gmail.com>
2025-06-25 21:11:26 +0800

    skeleton for loading

--- src/components/global/Loading.tsx
+++ src/components/global/Loading.tsx
@@ -1,13 +1,37 @@
 import React from "react";
-import logo from "@/public/global/logo-dark.svg";
-import Image from "next/image";
 
 const Loading = () => {
   return (
-    <div className="fixed top-0 left-0 w-full h-full backdrop-blur-md z-[60] flex items-center justify-center">
-      <div className="p-2 rounded-lg bg-none w-10 shadow-lg animate-rotate relative overflow-hidden flex items-center justify-center">
-        <div className="absolute bg-gradient-to-r from-accent-blue to-accent-yellow w-[200%] h-full z-0 animate-slide"></div>
-        <Image src={logo} alt="logo" className="z-10" />
+    <div className="w-full h-screen flex flex-col items-center justify-start overflow-hidden">
+      <div
+        className="w-full h-full flex flex-col items-center justify-start max-w-(--breakpoint-l-l) p-2
+              t:items-start t:p-4 gap-4 t:gap-8 overflow-hidden"
+      >
+        <div className="w-full flex flex-row items-start gap-2">
+          <div className="w-full min-w-72 flex flex-row items-center justify-between gap-2 t:w-6/12 l-l:w-4/12 mr-auto">
+            <div className="p-2 rounded-md bg-neutral-200 w-full min-h-14 h-14 animate-skeleton-fade" />
+            <div className="p-2 rounded-md bg-neutral-200 min-h-14 h-14 min-w-14 w-14 animate-skeleton-fade" />
+          </div>
+
+          <div className="w-full flex-row items-center justify-between gap-2 t:w-44 t:min-w-44 t:max-w-44 hidden t:flex">
+            <div className="p-2 rounded-md bg-neutral-200 w-full min-h-14 h-14 animate-skeleton-fade" />
+            <div className="p-2 rounded-md bg-neutral-200 min-h-14 h-14 min-w-14 w-14 animate-skeleton-fade" />
+          </div>
+
+          <div className="w-full flex-row items-center justify-between gap-2 t:w-44 t:min-w-44 t:max-w-44 hidden t:flex">
+            <div className="p-2 rounded-md bg-neutral-200 w-full min-h-14 h-14 animate-skeleton-fade" />
+            <div className="p-2 rounded-md bg-neutral-200 min-h-14 h-14 min-w-14 w-14 animate-skeleton-fade" />
+          </div>
+        </div>
+
+        <div className="w-full grid grid-cols-1 t:grid-cols-2 l-l:grid-cols-3 gap-4">
+          <div className="p-2 rounded-md bg-neutral-200 w-full min-h-36 h-36 animate-skeleton-fade" />
+          <div className="p-2 rounded-md bg-neutral-200 w-full min-h-36 h-36 animate-skeleton-fade" />
+          <div className="p-2 rounded-md bg-neutral-200 w-full min-h-36 h-36 animate-skeleton-fade" />
+          <div className="p-2 rounded-md bg-neutral-200 w-full min-h-36 h-36 animate-skeleton-fade" />
+          <div className="p-2 rounded-md bg-neutral-200 w-full min-h-36 h-36 animate-skeleton-fade" />
+          <div className="p-2 rounded-md bg-neutral-200 w-full min-h-36 h-36 animate-skeleton-fade" />
+        </div>
       </div>
     </div>
   );
