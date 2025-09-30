26480e0513c6a5e572c5bab30cdeff481a6fb8c3
rn-mgl <reinb2ra@gmail.com>
2025-09-30 21:12:42 +0800

    use use

--- app/auth/sending/page.tsx
+++ app/auth/sending/page.tsx
@@ -1,17 +1,20 @@
 "use client";
 
-import sending from "@/public/global/sending.svg";
 import Logo from "@/global/navigation/Logo";
+import sending from "@/public/global/sending.svg";
 import { getCSRFToken } from "@/src/utils/token";
 import axios from "axios";
 
 import Image from "next/image";
-import { useSearchParams } from "next/navigation";
-import { Suspense } from "react";
+import { use } from "react";
 
-const Message = () => {
-  const params = useSearchParams();
-  const type = params && params.get("type");
+const Sending = ({
+  searchParams,
+}: {
+  searchParams: Promise<{ type?: string }>;
+}) => {
+  const params = use(searchParams);
+  const type = params.type;
   const url = process.env.URL;
 
   const message = {
@@ -47,25 +50,6 @@ const Message = () => {
   };
 
   return (
-    <div className="w-full flex flex-col items-center justify-start gap-2 text-center">
-      <p className="font-bold t:text-lg">
-        {message[type as keyof object]["title"]}
-      </p>
-      <p className="">{message[type as keyof object]["body"]}</p>
-      {type === "verification" ? (
-        <button
-          onClick={resendVerificationMail}
-          className="p-2 rounded-md bg-neutral-900 text-neutral-50 px-4 mt-4 font-bold"
-        >
-          Resend
-        </button>
-      ) : null}
-    </div>
-  );
-};
-
-const Sending = () => {
-  return (
     <div className="w-full h-screen flex flex-col items-center justify-start bg-linear-to-b from-accent-purple/50 to-accent-blue/20">
       <div className="w-full h-full flex flex-col items-start justify-start max-w-(--breakpoint-l-l) p-4">
         <Logo url="/" type="dark" />
@@ -77,9 +61,20 @@ const Sending = () => {
               className="drop-shadow-md saturate-150 animate-fade"
             />
           </div>
-          <Suspense>
-            <Message />
-          </Suspense>
+          <div className="w-full flex flex-col items-center justify-start gap-2 text-center">
+            <p className="font-bold t:text-lg">
+              {message[type as keyof object]["title"]}
+            </p>
+            <p>{message[type as keyof object]["body"]}</p>
+            {type === "verification" ? (
+              <button
+                onClick={resendVerificationMail}
+                className="p-2 rounded-md bg-neutral-900 text-neutral-50 px-4 mt-4 font-bold"
+              >
+                Resend
+              </button>
+            ) : null}
+          </div>
         </div>
       </div>
     </div>
