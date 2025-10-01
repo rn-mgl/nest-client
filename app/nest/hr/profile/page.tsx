035092f47d156dc5d40222acf4be05c51cd4edbf
rn-mgl <reinb2ra@gmail.com>
2025-10-01 21:19:45 +0800

    hr abort controller

--- app/nest/hr/profile/page.tsx
+++ app/nest/hr/profile/page.tsx
@@ -28,27 +28,31 @@ const Profile = () => {
   const user = session?.user;
   const currentUser = user?.current ?? 0;
 
-  const getProfile = React.useCallback(async () => {
-    try {
-      if (user?.token) {
-        const { data: responseData } = await axios.get(
-          `${url}/hr/profile/${currentUser}`,
-          {
-            headers: {
-              Authorization: `Bearer ${user.token}`,
-            },
-            withCredentials: true,
-          }
-        );
+  const getProfile = React.useCallback(
+    async (controller?: AbortController) => {
+      try {
+        if (user?.token) {
+          const { data: responseData } = await axios.get(
+            `${url}/hr/profile/${currentUser}`,
+            {
+              headers: {
+                Authorization: `Bearer ${user.token}`,
+              },
+              withCredentials: true,
+              signal: controller?.signal,
+            }
+          );
 
-        if (responseData.profile) {
-          setProfile(responseData.profile);
+          if (responseData.profile) {
+            setProfile(responseData.profile);
+          }
         }
+      } catch (error) {
+        console.log(error);
       }
-    } catch (error) {
-      console.log(error);
-    }
-  }, [url, user?.token, currentUser]);
+    },
+    [url, user?.token, currentUser]
+  );
 
   const handleCanEditProfile = () => {
     setCanEditProfile((prev) => !prev);
@@ -59,7 +63,13 @@ const Profile = () => {
   };
 
   React.useEffect(() => {
-    getProfile();
+    const controller = new AbortController();
+
+    getProfile(controller);
+
+    return () => {
+      controller.abort();
+    };
   }, [getProfile]);
 
   return (
